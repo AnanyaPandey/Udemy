8e2b7747e8b6d0b63b7eae40f368774f9924a150
Ananya <anany.boss@gmail.com>
2023-08-28 11:02:46 +0530

    first_commit

---
+++ BugFiing.py
@@ -0,0 +1,12 @@
+'''
+my_answer = input("What is your answer?")
+answers = ['Yes', 'No', 'Yes' 'No' my_answer]
+
+
+my_answer = input(What is your answer?)
+answers = ['Yes', 'No', 'Yes', 'No', my_answer]
+
+
+my_answer = input["What is your answer?"]
+answers = ['Yes', 'No', 'Yes', 'No', my_answer]
+'''
---
+++ main.py
@@ -0,0 +1,5 @@
+import pandas
+import calculate
+print(calculate.x+calculate.y+calculate.z)
+
+
